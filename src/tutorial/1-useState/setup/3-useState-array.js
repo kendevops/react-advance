4f2d7a3a186ac54fe135165c2af361dab4418011
Kenneth <kendevops@gmail.com>
2021-01-04 22:22:25 +0100

    Made changes on the buttons added

--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -3,12 +3,17 @@ import { data } from "../../../data";
 
 const UseStateArray = () => {
   const [people, setPeople] = useState(data);
+  const removeItem = (id) => {
+    setPeople(people.filter((person) => person.id !== id));
+  };
   return (
     <>
       {people.map((person) => {
+        const { id, name } = person;
         return (
-          <div key={person.id} className="item">
-            <h4>{person.name}</h4>
+          <div key={id} className="item">
+            <h4>{name}</h4>
+            <button onClick={() => removeItem(id)}>remove</button>
           </div>
         );
       })}
