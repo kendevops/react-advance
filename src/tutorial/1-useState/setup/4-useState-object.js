cf42d858c047f5cce85ab410eea6e8ce6744ec01
Kenneth <kendevops@gmail.com>
2021-01-04 23:10:40 +0100

    worked on object for data

--- src/tutorial/1-useState/setup/4-useState-object.js
+++ src/tutorial/1-useState/setup/4-useState-object.js
@@ -1,7 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const UseStateObject = () => {
-  return <h2>useState object example</h2>;
+  const [data, setData] = useState({
+    name: "Kenneth",
+    age: 27,
+    message: "Where you feast is where you are gloryified",
+  });
+
+  const changeMessage = () => {
+    setData({...data, message: "It's been a nice time here"})
+  }
+  return (<>
+  <h2>{data.name}</h2>
+  <h4>{data.age}</h4>
+  <h4>{data.message}</h4>
+  <button onClick={changeMessage}>Change Message</button>
+  </>);
 };
 
 export default UseStateObject;
