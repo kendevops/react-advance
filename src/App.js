891f0319eb6a45afcb52e7c86173768983c326c9
Kenneth <kendevops@gmail.com>
2021-01-04 16:58:12 +0100

    worked on the array method

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react'
-import Setup from "./tutorial/1-useState/setup/1-error-example"
+import React from "react";
+import Setup from "./tutorial/1-useState/setup/3-useState-array";
 function App() {
   return (
-    <div className='container'>
+    <div className="container">
       <Setup />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/tutorial/1-useState/setup/3-useState-array.js
+++ src/tutorial/1-useState/setup/3-useState-array.js
@@ -1,8 +1,19 @@
-import React from 'react';
-import { data } from '../../../data';
+import React, { useState } from "react";
+import { data } from "../../../data";
 
 const UseStateArray = () => {
-  return <h2>useState array example</h2>;
+  const [people, setPeople] = useState(data);
+  return (
+    <>
+      {people.map((person) => {
+        return (
+          <div key={person.id} className="item">
+            <h4>{person.name}</h4>
+          </div>
+        );
+      })}
+    </>
+  );
 };
 
 export default UseStateArray;
